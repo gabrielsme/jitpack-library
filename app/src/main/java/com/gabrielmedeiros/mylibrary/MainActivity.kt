539d891d440dd8ecbf8a27c0a41922d1d796cd47
Gabriel Medeiros <gabrielsme@gmail.com>
2024-05-06 22:55:31 +0100

    Adicionando arquivos para o JitPack

--- app/src/main/java/com/gabrielmedeiros/mylibrary/MainActivity.kt
+++ app/src/main/java/com/gabrielmedeiros/mylibrary/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.gabrielmedeiros.my_library.LibUser
 import com.gabrielmedeiros.mylibrary.ui.theme.MyLibraryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val l = LibUser("name", "email")
             MyLibraryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
