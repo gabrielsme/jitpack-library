4da995082212ef294871d03b25d0e46ab9f76883
Gabriel Medeiros <gabrielsme@gmail.com>
2024-05-06 23:07:50 +0100

    Adicionando arquivos para gerar o build

--- my-library/build.gradle.kts
+++ my-library/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -21,6 +22,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            consumerProguardFiles("consumer-rules.pro")
         }
     }
     compileOptions {
@@ -40,4 +42,34 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+publishing {
+    publications {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.gabrielsme"
+                artifactId = "jitpack-library"
+                version = "1.0"
+
+//                from(components["java"])
+                pom {
+                    description = "DESCRIPTION"
+                }
+            }
+        }
+    }
+    repositories {               // << --- ADD This
+        mavenLocal()
+    }
 }
